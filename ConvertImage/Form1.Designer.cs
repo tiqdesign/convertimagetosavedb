99a1b2c80bdb31323808f3750459e9b94981acc6
Tarık KAYA <tiqqq24@gmail.com>
2019-10-14 13:27:59 +0300

    done

---
+++ ConvertImage/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace ConvertImage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_sec = new DevExpress.XtraEditors.SimpleButton();
+            this.btn_kaydet = new DevExpress.XtraEditors.SimpleButton();
+            this.btn_sifirla = new DevExpress.XtraEditors.SimpleButton();
+            this.btn_getir = new DevExpress.XtraEditors.SimpleButton();
+            this.pictureDbSet1 = new ConvertImage.PictureDbSet();
+            this.pb_secilen = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureDbSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_secilen)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_sec
+            // 
+            this.btn_sec.Location = new System.Drawing.Point(12, 12);
+            this.btn_sec.Name = "btn_sec";
+            this.btn_sec.Size = new System.Drawing.Size(266, 23);
+            this.btn_sec.TabIndex = 1;
+            this.btn_sec.Text = "Resim Seç";
+            this.btn_sec.Click += new System.EventHandler(this.btn_sec_Click);
+            // 
+            // btn_kaydet
+            // 
+            this.btn_kaydet.Location = new System.Drawing.Point(12, 267);
+            this.btn_kaydet.Name = "btn_kaydet";
+            this.btn_kaydet.Size = new System.Drawing.Size(266, 23);
+            this.btn_kaydet.TabIndex = 2;
+            this.btn_kaydet.Text = "Db ye kaydet";
+            this.btn_kaydet.Click += new System.EventHandler(this.btn_kaydet_Click);
+            // 
+            // btn_sifirla
+            // 
+            this.btn_sifirla.Location = new System.Drawing.Point(158, 296);
+            this.btn_sifirla.Name = "btn_sifirla";
+            this.btn_sifirla.Size = new System.Drawing.Size(120, 23);
+            this.btn_sifirla.TabIndex = 3;
+            this.btn_sifirla.Text = "Sıfırla";
+            this.btn_sifirla.Click += new System.EventHandler(this.btn_sifirla_Click);
+            // 
+            // btn_getir
+            // 
+            this.btn_getir.Location = new System.Drawing.Point(12, 296);
+            this.btn_getir.Name = "btn_getir";
+            this.btn_getir.Size = new System.Drawing.Size(125, 23);
+            this.btn_getir.TabIndex = 4;
+            this.btn_getir.Text = "Db den getir";
+            this.btn_getir.Click += new System.EventHandler(this.btn_getir_Click);
+            // 
+            // pictureDbSet1
+            // 
+            this.pictureDbSet1.DataSetName = "PictureDbSet";
+            this.pictureDbSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // pb_secilen
+            // 
+            this.pb_secilen.Location = new System.Drawing.Point(12, 41);
+            this.pb_secilen.Name = "pb_secilen";
+            this.pb_secilen.Size = new System.Drawing.Size(266, 220);
+            this.pb_secilen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_secilen.TabIndex = 5;
+            this.pb_secilen.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(290, 332);
+            this.Controls.Add(this.pb_secilen);
+            this.Controls.Add(this.btn_getir);
+            this.Controls.Add(this.btn_sifirla);
+            this.Controls.Add(this.btn_kaydet);
+            this.Controls.Add(this.btn_sec);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureDbSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_secilen)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.SimpleButton btn_sec;
+        private DevExpress.XtraEditors.SimpleButton btn_kaydet;
+        private DevExpress.XtraEditors.SimpleButton btn_sifirla;
+        private DevExpress.XtraEditors.SimpleButton btn_getir;
+        private PictureDbSet pictureDbSet1;
+        private System.Windows.Forms.PictureBox pb_secilen;
+
+    }
+}
+
---
+++ ConvertImage/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ConvertImage.PictureDbSetTableAdapters;
+
+namespace ConvertImage
+{
+    public partial class Form1 : DevExpress.XtraEditors.XtraForm
+    {
+        private Image img_secilen;
+        private PictureDbSet pbset = new PictureDbSet();
+        private PictureDbSetTableAdapters.PictureEditTableAdapter adapter = new PictureEditTableAdapter();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void btn_kaydet_Click(object sender, EventArgs e)
+        {
+            var pic_db = imagetobyte(img_secilen);
+            adapter.InsertQuery("Araba",pic_db);
+        }
+
+        private void btn_sec_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dosya =new OpenFileDialog();
+            dosya.ShowDialog();
+            var filename = dosya.FileName;
+            Bitmap b1 = new Bitmap(filename);
+            pb_secilen.Image = (Image) b1;
+            img_secilen = (Image)b1;
+        }
+
+        public static byte[] imagetobyte(Image img)
+        {
+            byte[] byteArray = new byte[0];
+            MemoryStream stream = new MemoryStream();
+            
+                img.Save(stream, System.Drawing.Imaging.ImageFormat.Jpeg);
+                stream.Close();
+
+                byteArray = stream.ToArray();
+            
+            return byteArray;
+        }
+
+        private void btn_sifirla_Click(object sender, EventArgs e)
+        {
+            pb_secilen.Image = null;
+        }
+
+        private void btn_getir_Click(object sender, EventArgs e)
+        {
+            var gelen_byte = adapter.GetPicture("Araba");
+            Image gelen_img = byteArrayToImage(gelen_byte);
+            pb_secilen.Image = gelen_img;
+        }
+
+        public Image byteArrayToImage(byte[] byteArrayIn)
+        {
+                MemoryStream ms = new MemoryStream(byteArrayIn);
+            Image returnImage = Image.FromStream(ms);
+            return returnImage;
+        }
+    }
+}
